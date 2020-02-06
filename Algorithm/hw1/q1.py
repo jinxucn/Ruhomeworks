d39f8bd8c245d4437aeff50a8126d20832bf1299
Jin <jin.xucn@outlook.com>
2020-02-05 20:56:52 -0500

    tc

--- Algorithm/hw1/q1.py
+++ Algorithm/hw1/q1.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
-# @Author: Jin X
-# @Date:   2020-02-04 16:31:04
-# @Last Modified by:   Jin X
-# @Last Modified time: 2020-02-04 16:57:24
+#!/usr/bin/env python
+# coding=utf-8
+'''
+@Author: Jin X
+@Date: 2020-02-02 20:50:23
+@LastEditTime : 2020-02-05 20:40:25
+'''
 import os
 from time import *
 
@@ -10,8 +12,8 @@ from time import *
 def naive3sum(data):
     count = 0
     for i in range(len(data)):
-        for j in range(i+1, len(data)):
-            for k in range(j+1, len(data)):
+        for j in range(i + 1, len(data)):
+            for k in range(j + 1, len(data)):
                 if data[i] + data[j] + data[k] == 0:
                     count += 1
     return count
@@ -29,7 +31,7 @@ def smart3sum(data):
 def _binarySearch(data, val, *args):
     if len(args) == 0:
         lo = 0
-        hi = len(data)
+        hi = len(data) - 1
         return _binarySearch(data, val, lo, hi)
     else:
         lo = args[0]
@@ -41,24 +43,25 @@ def _binarySearch(data, val, *args):
             if data[mid] == val:
                 return True
             elif val < data[mid]:
-                return _binarySearch(data, val, lo, mid-1)
+                return _binarySearch(data, val, lo, mid - 1)
             elif val > data[mid]:
-                return _binarySearch(data, val, mid+1, hi)
+                return _binarySearch(data, val, mid + 1, hi)
 
 
 if __name__ == "__main__":
     # a = _binarySearch([-67, -52, -43, -42, -37, -15, -6, -2,
-    #                    3, 6, 12, 42, 65, 124, 342, 502], 502)
+    #                    3, 6, 12, 42, 65, 124, 342, 502], 503)
     # print(a)
     # files = os.listdir("./Algorithm./hw1/hw1-1.data")
     # for f in files:
     #     print(f)
     a = []
-    with open(r"C:\Users\test1\Documents\Homework\Algorithm\hw1\hw1-1.data\8192int.txt", 'r') as f:
+    with open(r"C:\Users\test1\Documents\Homework\Algorithm\
+                hw1\hw1-1.data\8192int.txt", 'r') as f:
         for line in f.readlines():
             a.append(int(line))
     print(a)
     begint = time()
     print(smart3sum(a))
     endt = time()
-    print(endt-begint)
+    print(endt - begint)
